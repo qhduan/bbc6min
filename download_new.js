8937a2698ee396ce2affbd3fad55adb93ff788f4
QHDuan <mail@qhduan.com>
2016-03-19 11:54:48 +0800

    third

--- download_new.js
+++ download_new.js
@@ -18,21 +18,31 @@ function download_one () {
         page.open(url, function (status) {
             if (status == "success") {
                 var title = page.evaluate(function () {
+                    if ( !document.querySelectorAll(".widget-heading h3") )
+                        return null;
                     return document.querySelectorAll(".widget-heading h3")[1].textContent.trim();
                 });
                 var date = page.evaluate(function () {
+                    if ( !document.querySelector(".widget-bbcle-featuresubheader .details h3") )
+                        return null;
                     var d = Date.parse(document.querySelector(".widget-bbcle-featuresubheader .details h3").textContent.trim().split("/")[1]);
                     var now = new Date();
                     now.setTime(d);
                     return now.toISOString();
                 });
                 var audio = page.evaluate(function () {
+                    if ( !document.querySelector("a.bbcle-download-extension-mp3") )
+                        return null;
                     return document.querySelector("a.bbcle-download-extension-mp3").href;
                 });
                 var pdf =  page.evaluate(function () {
+                    if ( !document.querySelector(".bbcle-download-extension-pdf") )
+                        return null;
                     return document.querySelector(".bbcle-download-extension-pdf").href;
                 });
                 var description = page.evaluate(function () {
+                    if ( !document.querySelectorAll(".text p") || !document.querySelectorAll(".text p").length )
+                        return null;
                     var nodes = document.querySelectorAll(".text p");
                     var ret = [];
                     for (var i = 0; i < nodes.length; i++) {
@@ -40,22 +50,26 @@ function download_one () {
                     }
                     return ret.join("\n");
                 });
-                var obj = {
-                    title: title,
-                    description: description,
-                    date: date,
-                    audio: audio,
-                    pdf: pdf
-                };
-                console.log("解析了", url, obj);
-                ret.push(obj);
+                if (title && description && date && audio && pdf) {
+                    var obj = {
+                        title: title,
+                        description: description,
+                        date: date,
+                        audio: audio,
+                        pdf: pdf
+                    };
+                    console.log("解析了", url);
+                    ret.push(obj);
+                } else {
+                    console.log("解析错误", url);
+                }
             } else {
                 console.error("打开错误", url);
             }
             download_one();
         });
     } else {
-        fs.write("old_url.json", JSON.stringify(ret, null, 2), "w");
+        fs.write("new_url.json", JSON.stringify(ret, null, 2), "w");
         phantom.exit();
     }
 }
--- download_old.js
+++ download_old.js
@@ -18,21 +18,33 @@ function download_one () {
         page.open(url, function (status) {
             if (status == "success") {
                 var title = page.evaluate(function () {
+                    if ( !document.querySelector(".page-title") )
+                        return null;
                     return document.querySelector(".page-title").textContent;
                 });
                 var date = page.evaluate(function () {
+                    if ( !document.querySelector(".last-updated") )
+                        return null;
                     var d = Date.parse(document.querySelector(".last-updated").textContent.substr(16))
                     var now = new Date();
                     now.setTime(d);
                     return now.toISOString();
                 });
                 var audio = page.evaluate(function () {
+                    if ( !document.querySelector(".audio-link") )
+                        return null;
                     return document.querySelector(".audio-link").href;
                 });
                 var pdf =  page.evaluate(function () {
+                    if ( !document.querySelector(".pdf-link") )
+                        return null;
                     return document.querySelector(".pdf-link").href;
                 });
                 var words = page.evaluate(function () {
+                    if ( !document.querySelectorAll("dt") || !document.querySelectorAll("dt").length )
+                        return null;
+                    if ( !document.querySelectorAll("dd") || !document.querySelectorAll("dd").length )
+                        return null;
                     var ret = [];
                     var dt = document.querySelectorAll("dt");
                     var dd = document.querySelectorAll("dd");
@@ -45,6 +57,8 @@ function download_one () {
                     return ret;
                 });
                 var description = page.evaluate(function () {
+                    if ( !document.querySelectorAll("#story p") || !document.querySelectorAll("#story p").length )
+                        return null;
                     var nodes = document.querySelectorAll("#story p");
                     var ret = [];
                     for (var i = 0; i < nodes.length; i++) {
@@ -52,16 +66,20 @@ function download_one () {
                     }
                     return ret.join("\n");
                 });
-                var obj = {
-                    title: title,
-                    description: description,
-                    date: date,
-                    audio: audio,
-                    pdf: pdf,
-                    words: words
-                };
-                console.log("解析了", url, obj);
-                ret.push(obj);
+                if (title && description && date && audio && pdf && words) {
+                    var obj = {
+                        title: title,
+                        description: description,
+                        date: date,
+                        audio: audio,
+                        pdf: pdf,
+                        words: words
+                    };
+                    console.log("解析了", url);
+                    ret.push(obj);
+                } else {
+                    console.log("解析错误", url);
+                }
             } else {
                 console.error("打开错误", url);
             }
